fef5c1f3fbac8ff0cfaf64df802de2d62d62db54
Kevin Boyle <papershack@gmail.com>
2018-12-22 14:39:08 -0800

    add arrow function onSubmit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    state = {term: ''}
+    state = {
+        term: ''
+    }
 
+    onFormSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.term);
+    }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
             <div className="field">
                 <label>Image Search</label>
                 <input 
